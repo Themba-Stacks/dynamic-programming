952fee8dcf256face9da336c7f042a6b792a34b1
Themba Dombo <themba.dombo@gmail.com>
2021-07-16 06:58:25 +0200

    added can construct logic

---
+++ canConstruct/canConstruct-memo.js
@@ -0,0 +1,22 @@
+function canConstruct(target, wordBank, memo={}) {
+    if (target in memo ) return memo[target];
+    if (target === '') return true;
+
+    for (let word of wordBank) {
+        if(target.indexOf(word) === 0){
+            const prefix = target.slice(word.length);
+            if(canConstruct(prefix, wordBank, memo)) {
+                memo[target] = true;
+                return true;
+            }
+        }
+    }
+    
+    memo[target] = false;
+    return false;
+};
+
+console.log(canConstruct("abcdef", ["ab","abc", "cd", "def", "abcd"])); // true
+console.log(canConstruct("skateboard", ["bo", "rd" ,"ate", "t", "ska", "sk", "boar"]));//false
+console.log(canConstruct("enterapotentpot", ['a','p','ent','enter','ot','o','t'])); //true
+console.log(canConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", ["e","ee","eee","eeee","eeeee","eeeeee"])); //false
